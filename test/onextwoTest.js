4af2c746201483d92fd3ada6675aaf427324d047
Danny <33199566+dsanderson1@users.noreply.github.com>
2020-06-09 10:33:00 +0100

    Make unit tests independent to each other
    Reset the cache of the required module every time a test has finished.

--- test/onextwoTest.js
+++ test/onextwoTest.js
@@ -1,29 +1,41 @@
 //require the assert library with all the functions such as .equals.
 const assert = require('chai').assert;
 //require the actual javascript file with the function.
-const onextwo = require('../onextwo');
+
+function resets(){
+  delete require.cache[require.resolve('../onextwo')];
+}
 
 //5 tests that show if the input matches the expected input. Used .deepEqual() as it forces objects to be compared and .equal() doesn't
-describe('Test functionality', function(){
+describe('1st Test', function(){
   it('[{id:1}, {id:2}] should return [[{id:1}, {id:2}]]', function(){
+    const onextwo = require('../onextwo');
     let result = onextwo([{id:1}, {id:2}]);
-    //console.log(result);
     assert.deepEqual(result, [[{id:1}, {id:2}]]);
+    resets();
   });
   it('[{id:1}, {id:1}, {id:2}] should return [[{id:1}, {id:2}], [{id:1}, {id:2}], [{id:1}, {id:2}]]', function(){
+    const onextwo = require('../onextwo');
     let result = onextwo([{id:1}, {id:1}, {id:2}]);
-    assert.deepEqual(result, [[{id:1}, {id:2}], [{id:1}, {id:2}], [{id:1}, {id:2}]]);
+    assert.deepEqual(result, [[{id:1}, {id:2}], [{id:1}, {id:2}]]);
+    resets();
   });
-  it('[{id:1}, {id:1}, {id:2}, {id:2}, {id:3}] should return [[{id:1}, {id:2, {id:3}], [{id:1}, {id:2, {id:3}], [{id:1}, {id:2, {id:3}]]', function(){
+  it('[{id:1}, {id:1}, {id:2}, {id:2}, {id:3}] should return [[{id:1}, {id:2, {id:3}], [{id:1}, {id:2, {id:3}]]', function(){
+    const onextwo = require('../onextwo');
     let result = onextwo([{id:1}, {id:1}, {id:2}, {id:2}, {id:3}]);
-    assert.deepEqual(result, [[{id:1}, {id:2}, {id:3}], [{id:1}, {id:2}, {id:3}], [{id:1}, {id:2}, {id:3}], [{id:1}, {id:2}, {id:3}], [{id:1}, {id:2}, {id:3}]]);
+    assert.deepEqual(result, [[{id:1}, {id:2}, {id:3}], [{id:1}, {id:2}, {id:3}]]);
+    resets()
   });
-  it('[{id:4}] should return [[{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}]]', function(){
+  it('[{id:4}] should return [[{id:4}]]', function(){
+    const onextwo = require('../onextwo');
     let result = onextwo([{id:4}]);
-    assert.deepEqual(result, [[{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}]]);
+    assert.deepEqual(result, [[{id:4}]]);
+    resets();
   });
-  it('[{id:1} should return [[{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}]]', function(){
-    let result = onextwo([{id:1}]);
-    assert.deepEqual(result, [[{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}], [{id:1}, {id:2}, {id:3}, {id:4}]]);
+  it('[{id:2}, {id:2}, {id:4}, {id:6}] should return [[{id:2}, {id:4}, {id:6}], [{id:2}, {id:4}, {id:6}]]', function(){
+    const onextwo = require('../onextwo');
+    let result = onextwo([{id:2}, {id:2}, {id:4}, {id:6}]);
+    assert.deepEqual(result, [[{id:2}, {id:4}, {id:6}], [{id:2}, {id:4}, {id:6}]]);
+    resets();
   });
 });
